b5fee7ee6c31f76d909fe707c4c36517dbf32d74
Syafardilah <syafardilah@gmail.com>
2017-04-03 22:56:07 +0700

    fist commit

---
+++ Nilai.java
@@ -0,0 +1,10 @@
+public class mencariNilaiIpMahasiswa {
+    public static void main (String[] args){
+        String nilai;
+        final double n_ip;
+        Scanner input = new Scanner (System.in);
+		System.out.println("Menghitung Nilai IP Mahasiswa");
+        n_ip = (0.15 * n_tugas + 0.20 * n_kuis + 0.30 * n_uts + 0.35 * n_uas);
+        System.out.println(nama+" dengan NIM "+nim+" memiliki nilai akhir "+n_akhir);
+    }
+}
